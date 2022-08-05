16524c7a670d9a942bcd4be573a437641a2d73ea
jovanigomez <jovani.gomez@gmail.com>
2022-08-04 20:58:54 -0700

    adding console

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,20 @@
 
 $(document).ready(function() {
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-    
- 
-});
+    $(".saveBtn").on("click", function () { 
 
+   
+
+        console.log(this);
+   
+        var text = $(this).sibling(".description".val());
+   
+        var time = $(this).parent().attr("id");
+
+   
+        localStorage.setItem(time, text);
+
+
+    });
+
+});
