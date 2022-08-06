3193ac47e6e7387cf1ebad0a8902a52c68e7cc02
jovanigomez <jovani.gomez@gmail.com>
2022-08-05 23:18:44 -0700

    fixing time

--- assets/script.js
+++ assets/script.js
@@ -20,14 +20,14 @@ $(document).ready(function() {
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour1 .description").val(localStorage.getItem("hour1"));
+    $("#hour2 .description").val(localStorage.getItem("hour2"));
+    $("#hour3 .description").val(localStorage.getItem("hour3"));
+    $("#hour4 .description").val(localStorage.getItem("hour4"));
+    $("#hour5 .description").val(localStorage.getItem("hour5"));
+    $("#hour6 .description").val(localStorage.getItem("hour6"));
 
 
 
